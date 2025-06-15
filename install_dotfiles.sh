aa63e4a4a92e9ac209caaa6485e71aaf5d0deb39
edudscrc <eduardo.sa.coelho@gmail.com>
2025-06-15 20:12:08 -0300

    First Commit

---
+++ install_dotfiles.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/dotfiles
+CONFIG_DIR=~/.config
+
+declare -A DOTFILES
+DOTFILES=(
+    ["hypr"]="$CONFIG_DIR/hypr"
+    ["waybar"]="$CONFIG_DIR/waybar"
+    ["rofi"]="$CONFIG_DIR/rofi"
+    ["kitty"]="$CONFIG_DIR/kitty"
+    ["dunst"]="$CONFIG_DIR/dunst"
+)
+
+declare -A HOME_FILES
+HOME_FILES=(
+    [".bashrc"]="$HOME/.bashrc"
+)
+
+echo "Setting up dotfiles..."
+
+for folder in "${!DOTFILES[@]}"; do
+    TARGET="${DOTFILES[$folder]}"
+    SOURCE="$DOTFILES_DIR/$folder"
+
+    ln -sfn "$SOURCE" "$TARGET"
+    echo "Linked $SOURCE -> $TARGET"
+done
+
+for file in "${!HOME_FILES[@]}"; do
+    SOURCE="$DOTFILES_DIR/$file"
+
+    rm "$HOME/$file"
+    cp "$SOURCE" "$HOME"
+    echo "Copied $SOURCE -> $HOME"
+done
+
+hyprctl reload
+
+echo "Dotfiles setup complete!"
