6535bd3c75b1d2d337cd7f2ef2274661d790181f
edudscrc <eduardo.sa.coelho@gmail.com>
2025-08-15 09:48:29 -0300

    fixed weather module

--- waybar/assets/weather.py
+++ waybar/assets/weather.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from pyquery import PyQuery  # install using `pip install pyquery`
 import json
+
+from pyquery import PyQuery  # install using `pip install pyquery`
 
 ################################### CONFIGURATION ###################################
 location_id = "294e1ac420e35622f2a093ac2951485fa4c28cd2fe5b8a01da513a1b2bb809ca"
@@ -10,7 +11,7 @@ location_id = "294e1ac420e35622f2a093ac2951485fa4c28cd2fe5b8a01da513a1b2bb809ca"
 unit = "metric"  # metric or imperial
 
 # forcase type
-forecast_type = "Hourly" # Hourly or Daily
+forecast_type = "Hourly"  # Hourly or Daily
 
 ########################################## MAIN ##################################
 
@@ -23,10 +24,6 @@ html_data = PyQuery(url=url)
 
 # current temperature
 temp = html_data("span[data-testid='TemperatureValue']").eq(0).text()
-
-# status code
-status_code_class = html_data("#regionHeader").attr("class")
-status_code = str(status_code_class).split(" ")[2].split("-")[2]
 
 # min-max temperature
 temp_min = (
@@ -43,6 +40,6 @@ temp_max = (
 out_data = {
     "text": f"  {temp}C",
     "tooltip": f"Min: {temp_min}C\nMax: {temp_max}C",
-    "class": status_code,
 }
 print(json.dumps(out_data))
+
